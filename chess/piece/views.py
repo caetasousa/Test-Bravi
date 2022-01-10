2bec6039d744c27f41738f118e53c1bf7bff4167
root <root@LAPTOP-BM9S8AHN.localdomain>
2022-01-10 11:38:28 -0300

    Create route for consult piece and color

--- chess/piece/views.py
+++ chess/piece/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Piece
+from rest_framework.response import Response
+from .serializers import PiecesSerializer
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+
+class PieceId(viewsets.ViewSet):
+    serializer_class = PiecesSerializer
+    queryset = Piece.objects.all()
+
+    def list(self, request):
+
+        try:
+            dado = request.query_params
+            name, color = dado["name"], dado["color"]
+            data = get_object_or_404(self.queryset, name=name, color=color)
+        except:
+            return Response('You need send the name of the part and the color in query parameters')
+        
+        return Response({"id": data.id})
