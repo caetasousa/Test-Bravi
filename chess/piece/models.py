a5791a897f122cb05327e09dadf3ee4fac453218
root <root@LAPTOP-BM9S8AHN.localdomain>
2022-01-10 11:23:11 -0300

    Create Piece in models.py

--- chess/piece/models.py
+++ chess/piece/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Piece(models.Model):
+    color = (
+            ('black', 'Black'),
+            ('white', 'White'),
+        )
+
+    name = models.CharField(max_length=250)
+    color = models.CharField(max_length=10, choices=color)
+
+    def __str__(self):
+            return self.name
