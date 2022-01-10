71e01e522561f59358f5d534d17e7d661fa95974
root <root@LAPTOP-BM9S8AHN.localdomain>
2022-01-10 11:40:48 -0300

    Config django admin for register pieces

--- chess/piece/admin.py
+++ chess/piece/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.Piece)
+class ChessAdmin(admin.ModelAdmin):
+    list_display = ('name', 'color')
+
