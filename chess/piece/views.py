68b384d0ac1b960c11b06a43f37819bd0877c70a
root <root@LAPTOP-BM9S8AHN.localdomain>
2022-01-10 11:45:16 -0300

    Add PieceMove responsible for consulting id and coordinates

--- chess/piece/views.py
+++ chess/piece/views.py
@@ -3,6 +3,7 @@ from .models import Piece
 from rest_framework.response import Response
 from .serializers import PiecesSerializer
 from django.shortcuts import get_object_or_404
+from .chess_moves import move_knight_turn_one, move_knight_turn_two
 
 
 class PieceId(viewsets.ViewSet):
@@ -19,3 +20,23 @@ class PieceId(viewsets.ViewSet):
             return Response('You need send the name of the part and the color in query parameters')
         
         return Response({"id": data.id})
+
+
+class PieceMove(viewsets.ViewSet):
+    serializer_class = PiecesSerializer
+    queryset = Piece.objects.all()
+
+    def list(self, request):
+        
+        try:
+            dado = request.query_params
+            id, coordinate = dado["id"], dado["coordinate"]
+            data = get_object_or_404(self.queryset, id=id)
+            response = move_knight_turn_one(coordinate)
+
+            turn_two = move_knight_turn_two(response)
+            
+        except:
+            return Response('You need send the id and coordinate in query parameters')
+
+        return Response({'piece': data.name, 'color': data.color, 'moves_turn_one': response, 'moves_turn_two': turn_two})
--- chess/urls.py
+++ chess/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
-from chess.piece.views import  PieceId
+from chess.piece.views import PieceId, PieceMove
 
 router = routers.DefaultRouter()
 
 router.register('piece-id', PieceId, basename='piece-id')
+router.register('piece-move', PieceMove, basename='piece-move')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
